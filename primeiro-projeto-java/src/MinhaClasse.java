be566fec656f91060bf183f71cf53affed9a855e
Rafael30s <rafael30s@hotmail.com.br>
2024-07-30 15:23:42 -0300

    create first metod

--- primeiro-projeto-java/src/MinhaClasse.java
+++ primeiro-projeto-java/src/MinhaClasse.java
@@ -1,7 +1,27 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.print("Olá turma, sejam bem-vindos");
+        // imutável
+        final String BR = "Brasil";
+
+        // variaveis
+
+        int anoFabricacao = 2024;
+        boolean verdadeira = true;
+        anoFabricacao = 2023;
+
+        // nomes
+        String primeiroNome = "Rafael";
+        String segundoNome = "Santos";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
