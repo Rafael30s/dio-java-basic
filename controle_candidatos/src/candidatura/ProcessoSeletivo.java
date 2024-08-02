1ae6b7cc29550dc2b0b98174b5e254e2bbf4cf02
Rafael30s <rafael30s@hotmail.com.br>
2024-08-02 14:23:24 -0300

    add ProcessoSeletivo.java

---
+++ controle_candidatos/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,21 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("Processo seletivo");
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar para o candidato");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR COM CONTRA-PROPOSTA");
+        } else {
+            System.out.println("Aguardando resultado dos demais candidato");
+        }
+    }
+}
